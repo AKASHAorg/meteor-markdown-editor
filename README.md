5399f953847743485fe1f315de853bb16c918980
Marius Darila <marius.darila@gmail.com>
2015-10-23 15:04:35 +0300

    going live

---
+++ README.md
@@ -0,0 +1,2 @@
+# Meteor Markdown Editor
+
---
+++ package.js
@@ -0,0 +1,29 @@
+Package.describe({
+    name: 'akasha:markdown-editor',
+    version: '0.0.1',
+    // Brief, one-line summary of the package.
+    summary: 'simple markdown editor for meteor',
+    // URL to the Git repository containing the source code for this package.
+    git: 'git@github.com:AkashaProject/meteor-markdown-editor.git',
+    // By default, Meteor will default to using README.md for documentation.
+    // To avoid submitting documentation, set this field to null.
+    documentation: 'README.md'
+});
+
+Package.onUse(function (api) {
+    api.versionsFrom('1.2.0.2');
+    api.imply('templating');
+    api.use('underscore', 'client');
+    api.use('ecmascript');
+    api.use('less');
+    api.use('jquery');
+    api.use('blaze-html-templates');
+    api.use('chuangbo:marked@0.3.5_1');
+    api.use('twbs:bootstrap@3.3.5');
+    api.use('perak:codemirror@1.2.8');
+    api.use('ajduke:bootstrap-tagsinput@0.7.0');
+    api.addFiles('lib/client/views/markdown-editor.html', 'client');
+    api.addFiles('lib/client/views/markdown-editor.js', 'client');
+    api.addFiles('lib/client/styles/markdown-editor.less', 'client');
+    api.export('MarkdownEditor', 'client');
+});
