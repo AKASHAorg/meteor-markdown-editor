54381148d731ff1b2563d9e8f2e026e7ef13ea77
Marius Darila <marius.darila@gmail.com>
2015-10-23 16:33:23 +0300

    added new version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'akasha:markdown-editor',
-    version: '0.0.2',
+    version: '0.0.3',
     // Brief, one-line summary of the package.
     summary: 'simple markdown editor for meteor',
     // URL to the Git repository containing the source code for this package.
